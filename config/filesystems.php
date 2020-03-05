e17a254a9474033e8f823901cbf32ad54ca97ef3
Laravel Shift <shift@laravelshift.com>
2020-03-05 09:58:28 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them so you can review the commit diff for
    changes. Moving forward, you should use ENV variables or create a
    separate config file to allow the core config files to remain
    automatically upgradeable.

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Default Filesystem Disk
@@ -36,11 +37,12 @@ return [
     | may even configure multiple disks of the same driver. Defaults have
     | been setup for each driver as an example of the required options.
     |
-    | Supported Drivers: "local", "ftp", "s3", "rackspace"
+    | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
     */
 
     'disks' => [
+
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
@@ -48,7 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('storage'), //storage_path('app/public'),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
@@ -59,6 +61,24 @@ return [
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
         ],
+
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Symbolic Links
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the symbolic links that will be created when the
+    | `storage:link` Artisan command is executed. The array keys should be
+    | the locations of the links and the values should be their targets.
+    |
+    */
+
+    'links' => [
+        public_path('storage') => storage_path('app/public'),
+    ],
+
 ];
